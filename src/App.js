c145cac25fbeab618c5c1eec329d65e9a6a50d78
priyon777 <145787083+priyon777@users.noreply.github.com>
2023-12-09 04:14:57 +0530

    First react App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import LayoutContent from './components/LayoutContent';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <LayoutContent />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/LayoutContent.js
@@ -0,0 +1,7 @@
+import './LayoutContent.css';
+
+export default function LayoutContent(){
+    return (
+        <div className='layout-content'>This is content</div>
+    )
+}
